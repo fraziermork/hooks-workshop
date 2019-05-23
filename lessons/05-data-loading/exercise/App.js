d03f2ed874a2392f2eca38b20a04a0ddb43dd6a2
fraziermork <fraziermork@gmail.com>
2019-05-23 16:18:18 -0700

    Add data-loading hook

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -4,9 +4,24 @@ import { onAuthStateChanged } from 'app/utils'
 import LoggedIn from 'app/LoggedIn'
 import LoggedOut from 'app/LoggedOut'
 
+
+function useAuth() {
+  const [responseReceived, setResponseReceived] = useState(false);
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => onAuthStateChanged((newAuth) => {
+    setResponseReceived(true);
+    setAuth(newAuth)
+  }), []);
+
+  return [
+    responseReceived,
+    auth,
+  ];
+}
+
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const [authAttempted, auth] = useAuth();
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
